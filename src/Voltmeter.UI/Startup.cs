d99ef2571bf37838edd03c1564dfcb9094cafd63
Sander van Vliet <sander@codenizer.nl>
2018-12-28 16:24:18 +0100

    Change routing to set environment name parameter

--- src/Voltmeter.UI/Startup.cs
+++ src/Voltmeter.UI/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -51,7 +46,7 @@ namespace Voltmeter.UI
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{environmentName=?}");
             });
         }
     }
